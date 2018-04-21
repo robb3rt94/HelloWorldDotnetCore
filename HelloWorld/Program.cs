e6b3b3d8a9d637de8cf7d7d02fcf36eda415a40d
Robert Portilho <robert_portilho@hotmail.com>
2018-04-21 17:36:36 -0300

    Commit inicial

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nWhat is your name? ");
+            var nome = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.Write($"\nHello, {nome}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
